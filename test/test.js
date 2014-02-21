2f2f5ab347a751f67342c8356f1a0694c21587c2
Dominic Tarr <dominic.tarr@gmail.com>
2014-02-22 11:19:52 +1300

    test "" escaping, #1

--- test/test.js
+++ test/test.js
@@ -12,11 +12,13 @@ tape('simple', function (t) {
     ['c', '123', ''   , 'hello there'],
     ['','',''],
     ['d', ''   , 'aoeu oaeu j jn'  , 'hello, there'],
+    ['aa"b,bb"cc', 'hello', 'there']
   ]
 
   pull(
     pull.values(expected),
     csv.stringify(),
+    pull.through(console.log),
     csv.parse(),
     pull.collect(function (err, actual) {
       if(err) throw err
@@ -24,4 +26,16 @@ tape('simple', function (t) {
       t.end()
     })
   )
+})
+
+tape('parseLine', function (t) {
+
+  t.deepEqual(csv.parseLine('"aaa""bbb""ccc", 1, 2\n'), ['aaa"bbb"ccc', '1', '2'])
+  t.equal(csv.stringifyLine(['aaa"bbb"ccc', '1', '2']), '"aaa""bbb""ccc", 1, 2\n')
+
+  t.deepEqual(csv.parseLine('"a,aa""b,bb""c,cc", 1, 2\n'), ['a,aa"b,bb"c,cc', '1', '2'])
+
+
+  t.end()
+
 })
